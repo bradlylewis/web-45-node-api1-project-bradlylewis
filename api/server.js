69900700079936fa0dd3764bd45552faacaa2500
Bradly <bradlylewisdev@gmail.com>
2021-08-30 18:35:40 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,11 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Model = require('./users/model.js')
+const server = express()
+
+server.use(express.json())
+
+
+
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
