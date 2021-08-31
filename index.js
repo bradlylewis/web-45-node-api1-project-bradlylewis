d5e1dc27db14d5b2714e8095e138704a111dc5b4
Bradly <bradlylewisdev@gmail.com>
2021-08-30 20:49:23 -0400

    complete mvp

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-server.listen(5000, () => {
+server.listen(port, () => {
   console.log('listening on port 5000')
 })
